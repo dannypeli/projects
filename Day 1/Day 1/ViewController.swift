701cb6c94bc99c809878d9e9383396b547242891
dannypeli <105387734+dannypeli@users.noreply.github.com>
2022-05-11 14:33:10 -0500

    Add files via upload

---
+++ Day 1/Day 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day 1
+//
+//  Created by period8 on 8/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
